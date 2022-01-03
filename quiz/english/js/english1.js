42708a73fad138d5f65fc94df1dd42bfa2d697c0
Muhammad Alfa Ridzi <76870692+ridzidev@users.noreply.github.com>
2022-01-04 00:03:49 +0700

    quiz

---
+++ quiz/english/js/english1.js
@@ -0,0 +1,128 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "I and my friends … in the library. We read some books",
+    answer: "are",
+    options: [
+      "am",
+      "is",
+      "have",
+      "are"
+    ]
+  },
+    {
+    numb: 2,
+    question: "She … not work because she has the flu.",
+    answer: "does",
+    options: [
+      "is",
+      "does",
+      "do",
+      "be"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Alina … song every night.",
+    answer: "sings",
+    options: [
+      "sings",
+      "sing",
+      "is",
+      "does"
+    ]
+  },
+    {
+    numb: 4,
+    question: "My father … tea every morning.",
+    answer: "drinks",
+    options: [
+      "drink",
+      "drinks",
+      "drinking",
+      "is"
+    ]
+  },
+    {
+    numb: 5,
+    question: "They … a test every week",
+    answer: "have",
+    options: [
+      "does",
+      "has",
+      "are",
+      "have"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Dolph: Please call me if you need.<br>Jack: No. I … need your help.",
+    answer: "do not",
+    options: [
+      "do not",
+      "does",
+      "not",
+      "am not"
+    ]
+  },
+  {
+    numb: 7,
+    question: "She is a student. She … at school.",
+    answer: "studies",
+    options: [
+      "studying",
+      "study",
+      "studies",
+      "does"
+    ]
+  },
+  {
+    numb: 8,
+    question: "We … soccer match.",
+    answer: "watch",
+    options: [
+      "doing",
+      "watching",
+      "watches",
+      "watch"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Gina cooks fried rice. It … amazing.",
+    answer: "is",
+    options: [
+      "does",
+      "do",
+      "are",
+      "is"
+    ]
+  },
+  {
+    numb: 10,
+    question: "My brother rides a bike to school …",
+    answer: "every day",
+    options: [
+      "every day",
+      "last day",
+      "next week",
+      "next time"
+    ]
+  },
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
